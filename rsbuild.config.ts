24b47e0de6b51ba983c4bc2c270ed6a0b271d822
baicorp <bagusatoki@gmail.com>
2024-08-31 19:30:45 +0700

    improve

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,4 +3,8 @@ import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
   plugins: [pluginReact()],
+  html: {
+    title: "BaiStream",
+    favicon: "./src/favicon-32x32.png",
+  },
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { SetStateAction, useState } from "react";
+import { SetStateAction, useMemo, useState } from "react";
 import MovieIcon from "./component/svg/Movie";
 import SubtitleIcon from "./component/svg/Subtitle";
 
 type CurrentPlay = {
-  currentVideo: string;
-  currentSubtitle: string;
+  currentVideo: {
+    src: string;
+    name: string;
+  };
+  currentSubtitle: {
+    src: string;
+    name: string;
+  };
 };
 
 type Tabs = "videos" | "subtitles";
@@ -16,50 +22,86 @@ const ACCEPTEDFILE = {
 
 export default function App() {
   const [currentPlay, setCurrentPlay] = useState<CurrentPlay>({
-    currentVideo: "",
-    currentSubtitle: "",
+    currentVideo: {
+      src: "",
+      name: "",
+    },
+    currentSubtitle: {
+      src: "",
+      name: "",
+    },
   });
 
   return (
-    <main className="flex flex-col justify-center items-center gap-16 h-dvh px-14 py-10 border-2">
-      <h1 className="font-black text-zinc-900 text-6xl">MOVIE TIME 📺</h1>
-      <div className="w-full max-w-6xl flex gap-8">
-        <div className="basis-[70%] shrink-0 grow-0 overflow-hidden bg-black rounded-lg">
-          <video
-            controls
-            autoPlay
-            className="aspect-video w-full"
-            src={currentPlay.currentVideo}
-          >
-            Your browser does not support the video tag.
-            <track
-              src={currentPlay.currentSubtitle}
-              label="Indonesian"
-              kind="subtitles"
-              default
-            />
-          </video>
+    <>
+      <main className="flex justify-center items-center h-dvh px-14 py-10">
+        <div className="w-full max-w-6xl flex flex-col gap-8">
+          <h1 className="text-center font-bold text-8xl">MOVIE TIME 📺</h1>
+          <div className="flex justify-between gap-8">
+            <div className="basis-[70%] overflow-hidden bg-black rounded-lg">
+              <VideoPlayer
+                videoSource={currentPlay.currentVideo.src}
+                trackSource={currentPlay.currentSubtitle.src}
+              />
+            </div>
+            <div className="basis-[30%] overflow-hidden">
+              <Tabs setPlayer={setCurrentPlay} currentPlay={currentPlay} />
+            </div>
+          </div>
         </div>
-        <div className="basis-[30%] shrink-0 grow-0 overflow-hidden">
-          <Tabs setPlayer={setCurrentPlay} />
-        </div>
-      </div>
-    </main>
+      </main>
+    </>
+  );
+}
+
+function VideoPlayer({
+  videoSource,
+  trackSource,
+  className,
+}: {
+  videoSource: string;
+  trackSource: string;
+  className?: React.HTMLAttributes<HTMLVideoElement>["className"];
+}) {
+  return (
+    <video
+      controls
+      autoPlay
+      className={`aspect-video w-full ${className}`}
+      src={videoSource}
+    >
+      Your browser does not support the video tag.
+      <track src={trackSource} kind="subtitles" default />
+    </video>
   );
 }
 
 function Tabs({
+  currentPlay,
   setPlayer,
 }: {
+  currentPlay: CurrentPlay;
   setPlayer: React.Dispatch<SetStateAction<CurrentPlay>>;
 }) {
   const [tabs, setTabs] = useState<Tabs>("videos");
   const [videos, setVideos] = useState<FileList | null>(null);
   const [subtitles, setSubtitles] = useState<FileList | null>(null);
 
+  const videoFiles: File[] | null = useMemo(() => {
+    if (videos === null) return null;
+    return Array.from(videos).sort((a, b) => a.name.localeCompare(b.name));
+  }, [videos]);
+
+  const subtitleFiles: File[] | null = useMemo(() => {
+    if (subtitles === null) return null;
+    return Array.from(subtitles).sort((a, b) => a.name.localeCompare(b.name));
+  }, [subtitles]);
+
+  const files = tabs === "videos" ? videoFiles : subtitleFiles;
+
   return (
     <div className="flex flex-col h-full">
-      <div className="flex bg-black p-1 rounded-xl">
+      <div className="flex bg-zinc-100 p-1 rounded-xl">
         <TabItem tabName="videos" tabs={tabs} setState={setTabs} />
         <TabItem tabName="subtitles" tabs={tabs} setState={setTabs} />
       </div>
@@ -79,31 +121,12 @@ function Tabs({
           />
         )}
       </div>
-      <div className="grow">
-        {tabs === "videos" && (
-          <div
-            className={`h-full border-r-2 border-b-2 border-l-2 border-zinc-500 border-dashed rounded-lg`}
-          >
-            {videos !== null ? (
-              <Lists list={Array.from(videos)} setState={setPlayer} />
-            ) : (
-              <div className="h-full flex justify-center items-center text-gray-500">
-                <p>Empty</p>
-              </div>
-            )}
-          </div>
-        )}
-        {tabs === "subtitles" && (
-          <div
-            className={`h-full border-r-2 border-b-2 border-l-2 border-zinc-500 border-dashed rounded-lg`}
-          >
-            {subtitles !== null ? (
-              <Lists list={Array.from(subtitles)} setState={setPlayer} />
-            ) : (
-              <div className="h-full flex justify-center items-center text-gray-500">
-                <p>Empty</p>
-              </div>
-            )}
+      <div className="grow lg:max-h-[255.34px] xl:max-h-[349.84px] overflow-x-hidden overflow-y-auto border-r-2 border-b-2 border-l-2 border-zinc-500 border-dashed rounded-lg">
+        {files !== null ? (
+          <Lists list={files} setState={setPlayer} currentPlay={currentPlay} />
+        ) : (
+          <div className="h-full flex justify-center items-center text-gray-500">
+            <p>Empty</p>
           </div>
         )}
       </div>
@@ -123,8 +146,8 @@ function TabItem({
   return (
     <button
       className={`flex-1 px-6 font-bold py-1.5 rounded-lg ${
-        tabs === tabName ? "bg-white text-slate-950" : "text-white"
-      }`}
+        tabs === tabName ? "bg-black text-white" : "text-black"
+      } transition-all`}
       onClick={() => {
         setState(tabName);
       }}
@@ -144,10 +167,10 @@ function InputFile({
   setState: React.Dispatch<SetStateAction<FileList | null>>;
 }) {
   return (
-    <div className="border-2 border-zinc-500 border-dashed rounded-lg overflow-x-hidden overflow-y-auto">
+    <div className="hover:bg-zinc-800 border-2 border-zinc-500 border-dashed rounded-lg overflow-x-hidden overflow-y-auto">
       <label
         htmlFor="inputFile"
-        className="bg-zinc-200 py-2 font-bold flex gap-1 justify-center items-center cursor-pointer"
+        className="py-2 font-bold flex gap-1 justify-center items-center cursor-pointer"
       >
         {label.includes("video") ? <MovieIcon /> : <SubtitleIcon />}
         <span>{label}</span>
@@ -168,39 +191,47 @@ function InputFile({
 
 function Lists({
   list,
+  currentPlay,
   setState,
 }: {
   list: File[];
+  currentPlay: CurrentPlay;
   setState: React.Dispatch<SetStateAction<CurrentPlay>>;
 }) {
   return (
-    <ol className="flex flex-col">
-      {list.map((data, index) => (
-        <li
-          key={index}
-          className={`p-2 cursor-pointer text-sm flex items-center gap-1 ${
-            index % 2 !== 0 && "bg-zinc-200"
-          }`}
-          title={data.name}
-          onClick={() => {
-            setState((prev) => ({
-              ...prev,
-              [data.type === "video/x-matroska"
-                ? "currentVideo"
-                : "currentSubtitle"]: URL.createObjectURL(data),
-            }));
-          }}
-        >
-          <div className="aspect-square w-6">
-            {data.type === "video/x-matroska" ? (
-              <MovieIcon />
-            ) : (
-              <SubtitleIcon />
-            )}
-          </div>
-          <span className="line-clamp-1">{data.name}</span>
-        </li>
-      ))}
+    <ol className="divide-y-2 divide-zinc-600">
+      {list.map((data, index) => {
+        const isVideo = data.type.includes("video");
+        return (
+          <li
+            key={index}
+            className={`p-2 cursor-pointer text-sm flex items-center gap-1 ${
+              currentPlay[isVideo ? "currentVideo" : "currentSubtitle"][
+                "name"
+              ] === data.name
+                ? "bg-zinc-600"
+                : "bg-none"
+            }`}
+            title={data.name}
+            onClick={() => {
+              setState((prev) => {
+                return {
+                  ...prev,
+                  [isVideo ? "currentVideo" : "currentSubtitle"]: {
+                    src: URL.createObjectURL(data),
+                    name: data.name,
+                  },
+                };
+              });
+            }}
+          >
+            <div className="aspect-square w-6">
+              {data.type.includes("video") ? <MovieIcon /> : <SubtitleIcon />}
+            </div>
+            <span className="line-clamp-1">{data.name}</span>
+          </li>
+        );
+      })}
     </ol>
   );
 }
