7ee54b332ec4c824ac36179ae65c5e6b8f818adf
baicorp <bagusatoki@gmail.com>
2024-09-01 15:23:50 +0700

    add meta description

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -6,5 +6,9 @@ export default defineConfig({
   html: {
     title: "BaiStream",
     favicon: "./src/favicon-32x32.png",
+    meta: {
+      description:
+        "Share videos with sound on Google Meet with ease. Our website allows you to play local videos and share them seamlessly during your calls.",
+    },
   },
 });
